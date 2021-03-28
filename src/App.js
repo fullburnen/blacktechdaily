622e2f4ff3e273bf054e59f29cffb876b5ac159d
Joe Warren <joe.warren7@protonmail.com>
2021-03-27 23:47:30 -0500

    style: add spinner when loading

--- src/App.js
+++ src/App.js
@@ -3,31 +3,35 @@ import Footer from './components/footer/footer';
 import Button from './components/button/button';
 import './App.css';
 
-import ArticlesFeed from './components/articles-feed/articles-feed'
+import ArticlesFeed from './components/articles-feed/articles-feed';
 
 const App = () => {
   const [mode, setMode] = useState(localStorage.getItem('cachedMode') || '');
   const modeText = mode === 'light' ? 'dark' : 'light';
 
   useEffect(() => {
-    localStorage.setItem('cachedMode', mode)
-  }, [mode])
+    localStorage.setItem('cachedMode', mode);
+  }, [mode]);
 
-  const toggleMode = () => mode === 'light' ? setMode('dark') : setMode('light');
+  const toggleMode = () =>
+    mode === 'light' ? setMode('dark') : setMode('light');
 
   return (
     <div className={`container ${mode}-container`}>
       <div className={`content ${mode}-content`}>
-        <div className="header-top-row">
-          <h1 className="header-title"> Black Tech Daily </h1>
+        <div className='header-top-row'>
+          <h1 className='header-title'> Black Tech Daily </h1>
           <Button toggleMode={toggleMode} modeText={modeText} />
         </div>
-          <h2 className="header-subtitle"> Your Daily Source for News on Blacks in Tech and Business </h2>
-        <ArticlesFeed mode={mode}/>
+        <h2 className='header-subtitle'>
+          {' '}
+          Your Daily Source for News on Blacks in Tech and Business{' '}
+        </h2>
+        <ArticlesFeed mode={mode} />
       </div>
-      <Footer mode={mode}/>
+      <Footer mode={mode} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/articles-feed/articles-feed.jsx
+++ src/components/articles-feed/articles-feed.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import moment from 'moment';
+import CircularProgress from '@material-ui/core/CircularProgress';
+// import WbSunnyIcon from '@material-ui/icons/WbSunny';
+// import Brightness3Icon from '@material-ui/icons/Brightness3';
 import { articlesQuery } from './articles.config';
 import './articles-feed.css';
 
@@ -9,31 +12,39 @@ const ArticleFeed = props => {
 
   useEffect(() => {
     axios.get(articlesQuery).then(res => {
-      setArticles(res.data.articles)
-    })
+      setArticles(res.data.articles);
+    });
   }, []);
 
-  const uniqueArticles = Array.from(new Set(articles.map(article => article.title)))
-    .map(title => {
-      return articles.find(article => article.title === title)
-    })
+  const uniqueArticles = Array.from(
+    new Set(articles.map(article => article.title))
+  ).map(title => {
+    return articles.find(article => article.title === title);
+  });
 
   return articles.length > 0 ? (
-    uniqueArticles
-      .map((article, index) => {
-        return (
-          <p className="article" key={index}>
-            <span className={`article-header-${props.mode}`}>
-              Published {article.source.name ? `by ${article.source.name}` : null} {moment(article.publishedAt).fromNow()}
-            </span>
-            <br />
-            <a className={`article-title-${props.mode}`} href={article.url} target="_blank" rel="noopener noreferrer">
-              {article.title}
-            </a>
-          </p>
-        )
-      })
-  ) : 'Loading...'
-}
+    uniqueArticles.map((article, index) => {
+      return (
+        <p className='article' key={index}>
+          <span className={`article-header-${props.mode}`}>
+            Published {article.source.name ? `by ${article.source.name}` : null}{' '}
+            {moment(article.publishedAt).fromNow()}
+          </span>
+          <br />
+          <a
+            className={`article-title-${props.mode}`}
+            href={article.url}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {article.title}
+          </a>
+        </p>
+      );
+    })
+  ) : (
+    <CircularProgress />
+  );
+};
 
 export default ArticleFeed;
